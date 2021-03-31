871302c45ccd2b1a0d71c91b3fdb3f385b5d7640
Mashal Abbas <mashal@Mashals-MacBook-Air.local>
2021-03-31 23:35:35 +0500

    mid-database creation

--- Carletproject/Carletapp/admin.py
+++ Carletproject/Carletapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from Carletapp.models import Carlet_User, Vehicle_detail
 # Register your models here.
+admin.site.register(Carlet_User)
+admin.site.register(Vehicle_detail)
--- Carletproject/Carletapp/models.py
+++ Carletproject/Carletapp/models.py
@@ -1,30 +1,39 @@
 from django.db import models
 from django.conf import settings 
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 import uuid
 
 # Create your models here.
 class Carlet_User (models.Model):
     carletuser_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(
-      settings.AUTH_USER_MODEL, # new
-      on_delete=models.CASCADE
-    )
-    phone_number = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField()
     picture = models.ImageField()
     isVerified = models.BooleanField()
     isBanned = models.BooleanField()
     isSuperadmin = models.BooleanField()
     permanentBan = models.BooleanField()
-    tempBan = models.DateField(null = True)
+    tempBan = models.DateField(blank = True, null = True)
     wallet = models.IntegerField()
+    def __str__(self):
+      return self.user.username
 
 
-# class Vehicle_detail(models.Model):
-#     vehicle_id = models.EmailField(max_length=100)
 
-#     def __str__(self):
-#         return self.vehicle_id
+class Vehicle_detail(models.Model):
+    vehicle_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    vehicle_name = models.CharField(max_length = 100)
+    vehicle_street_address = models.TextField(max_length = 300)
+    vehicle_city = models.CharField(max_length = 100)
+    vehicle_state = models.CharField(max_length = 100)
+    vehicle_zip = models.CharField(max_length = 100, blank = True, null = True)
+    vehicle_latitude = models.DecimalField(max_digits=19, decimal_places=10,blank = True, null = True)
+    vehicle_longitude = models.DecimalField(max_digits=19, decimal_places=10,blank = True, null = True)
+
+
+    def __str__(self):
+        return (self.vehicle_name +" "+ str(self.vehicle_id))
 
 
 # class Vehicle_document(models.Model):
