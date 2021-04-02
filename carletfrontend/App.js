5c3d4179dbe29e0273d79db57009e1c5e1f356e8
Shahpar Khan <shahparkhan@Shahpars-MacBook-Pro.local>
2021-04-02 21:56:05 +0500

    New modules added to incorporate fonts. Custom Font Nunito added.

--- carletfrontend/App.js
+++ carletfrontend/App.js
@@ -1,17 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading'
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
 import Welcome from './screens/welcome'
 import Login from './screens/login'
 import Signup from './screens/signup'
 // import SvgComponent from './svgcomponents/yellowvector'
 
+const getFonts = () => {
+  return Font.loadAsync({
+    'Nunito-Black': require('./shared/fonts/Nunito-Black.ttf'),
+    'Nunito-BlackItalic': require('./shared/fonts/Nunito-BlackItalic.ttf'),
+    'Nunito-Bold': require('./shared/fonts/Nunito-Bold.ttf'),
+    'Nunito-BoldItalic': require('./shared/fonts/Nunito-BoldItalic.ttf'),
+    'Nunito-ExtraBold': require('./shared/fonts/Nunito-ExtraBold.ttf'),
+    'Nunito-ExtraBoldItalic': require('./shared/fonts/Nunito-ExtraBoldItalic.ttf'),
+    'Nunito-ExtraLight': require('./shared/fonts/Nunito-ExtraLight.ttf'),
+    'Nunito-ExtraLightItalic': require('./shared/fonts/Nunito-ExtraLightItalic.ttf'),
+    'Nunito-Italic': require('./shared/fonts/Nunito-Italic.ttf'),
+    'Nunito-Light': require('./shared/fonts/Nunito-Light.ttf'),
+    'Nunito-LightItalic': require('./shared/fonts/Nunito-LightItalic.ttf'),
+    'Nunito-Regular': require('./shared/fonts/Nunito-Regular.ttf'),
+    'Nunito-SemiBold': require('./shared/fonts/Nunito-SemiBold.ttf'),
+    'Nunito-SemiBoldItalic': require('./shared/fonts/Nunito-SemiBoldItalic.ttf')
+  })
+}
+
+
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome />
-    </View>
-  );
+  const [fontLoaded, setFontsLoaded] = useState(false)
+
+  if (fontLoaded) {
+    return (
+      <View style={styles.container}>
+        <Welcome />
+      </View>
+    )
+  } else {
+      return (
+        <AppLoading 
+          startAsync = {getFonts}
+          onFinish = {() => setFontsLoaded(true)}
+          onError = {console.warn}
+        />
+      )
+  }
+
+  
+
+  
 }
 
 const styles = StyleSheet.create({
@@ -19,6 +58,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
