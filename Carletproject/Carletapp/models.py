d5a9ad2b1f273d9607f79dbed31516903e594613
monum <abdulmonum@gmail.com>
2021-04-03 00:10:23 +0500

    Added 5 database tables

--- Carletproject/Carletapp/models.py
+++ Carletproject/Carletapp/models.py
@@ -36,50 +36,62 @@ class Vehicle_detail(models.Model):
         return (self.vehicle_name +" "+ str(self.vehicle_id))
 
 
-# class Vehicle_document(models.Model):
-#     vehicledoc_id = models.EmailField(max_length=100)
-
-#     def __str__(self):
-#         return self.vehicledoc_id
 
 
-# class Vehicle_Location(models.Model):
-#     vehicleloc_id = models.EmailField(max_length=100)
-
-#     def __str__(self):
-#         return self.vehicleloc_id
-
-
-# class User_document(models.Model):
-#     userdoc_id = models.EmailField(max_length=100)
-
-#     def __str__(self):
-#         return self.userdoc_id
+class User_document(models.Model):
+    user_doc_id = models.OneToOneField(Carlet_User, on_delete=models.CASCADE, primary_key=True)
+    nic = models.CharField(max_length = 13)
+    nic_picture = models.ImageField()
+    driver_license = models.CharField(max_length = 50)
+    driver_license_picture = models.ImageField()
+    account_number = models.CharField(max_length = 24)
 
 
-# class User_History(models.Model):
-#     userhis_id = models.EmailField(max_length=100)
-
-#     def __str__(self):
-#         return self.userhis_id
+    def __str__(self):
+        return str(self.user_doc_id)
 
 
-# class Rating(models.Model):
-#     rating_id = models.EmailField(max_length=100)
+class Trip_detail(models.Model):
+    trip_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner_id = models.ForeignKey(Carlet_User, on_delete=models.CASCADE, related_name ='owner_id')
+    renter_id = models.ForeignKey(Carlet_User, on_delete=models.CASCADE, related_name ='renter_id')
+    vehicle_id = models.ForeignKey(Vehicle_detail, on_delete=models.CASCADE)
+    pickup_date = models.DateField()
+    dropoff_date = models.DateField()
+    duration = models.PositiveIntegerField()
+    cost = models.PositiveIntegerField()
 
-#     def __str__(self):
-#         return self.rating_id
+    def __str__(self):
+        return str(self.trip_id)
+    
 
 
-# class Voucher(models.Model):
-#     voucher_id = models.EmailField(max_length=100)
+class Rating(models.Model):
+    rating_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    rater_id = models.OneToOneField(Carlet_User, on_delete=models.CASCADE, related_name= 'rater_id')
+    rated_id = models.OneToOneField(Carlet_User, on_delete= models.CASCADE, related_name = 'rated_id')
+    rating_points = models.DecimalField(max_digits = 1, decimal_places = 1)
+    review = models.TextField(max_length = 300)
+    isRenter = models.BooleanField()
 
-#     def __str__(self):
-#         return self.voucher_id
+    class Meta:
+        unique_together = (('rater_id', 'rated_id'),)
 
 
-# class Trip_history(models.Model):
-#     trip_id = models.EmailField(max_length=100)
+    def __str__(self):
+        return str(self.rater_id)
 
-#     def __str__(self):
-#         return self.trip_id
+    
+
+class Voucher(models.Model):
+    voucher_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.PositiveIntegerField()
+    carlet_user_id = models.ForeignKey(Carlet_User, on_delete=models.CASCADE, related_name='carletuser')
+    status = models.BooleanField()
+    issue_date = models.DateField()
+    due_date = models.DateField()
+
+    def __str__(self):
+        return str(self.voucher_id)
+
+
