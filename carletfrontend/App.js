b7a6dc11abbc5912d705d2cc177e9a9485e4692f
Adnan Abbas <adnan.abbas5212@gmail.com>
2021-04-03 12:22:02 +0500

    trial push

--- carletfrontend/App.js
+++ carletfrontend/App.js
@@ -47,9 +47,11 @@ export default function App() {
 
   if (fontLoaded) {
     return (
+     
       <View style={styles.container}>
-        <Welcome />
+        <Login />
       </View>
+      
     )
   } else {
       return (
@@ -65,6 +67,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
